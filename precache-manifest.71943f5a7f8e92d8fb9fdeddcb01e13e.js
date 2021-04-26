058221094db276582b2eabbd9c19b0043c9eb262
Tair_Uralov <tair.uralov99@gmail.com>
2021-04-27 01:03:45 +0600

    Updates

---
+++ precache-manifest.71943f5a7f8e92d8fb9fdeddcb01e13e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546f1e2708942f882717633117fc97da",
+    "url": "/plagiarism-detection/index.html"
+  },
+  {
+    "revision": "6ee33dbb6dacd3241546",
+    "url": "/plagiarism-detection/static/css/2.0b04df95.chunk.css"
+  },
+  {
+    "revision": "248edb3440c251762c15",
+    "url": "/plagiarism-detection/static/css/main.5221bab1.chunk.css"
+  },
+  {
+    "revision": "6ee33dbb6dacd3241546",
+    "url": "/plagiarism-detection/static/js/2.51e2e4a4.chunk.js"
+  },
+  {
+    "revision": "ddf0c79af7b61d2d74b12bbc56535a4f",
+    "url": "/plagiarism-detection/static/js/2.51e2e4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248edb3440c251762c15",
+    "url": "/plagiarism-detection/static/js/main.9a210248.chunk.js"
+  },
+  {
+    "revision": "030accf96d62e0b9ee73",
+    "url": "/plagiarism-detection/static/js/runtime-main.90ba4146.js"
+  },
+  {
+    "revision": "66acef3e25a1c860764576ecb7add04c",
+    "url": "/plagiarism-detection/static/media/12.66acef3e.jpg"
+  },
+  {
+    "revision": "5e15051a726bcf6b2c8f5c5db36d72ce",
+    "url": "/plagiarism-detection/static/media/334.5e15051a.jpg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/plagiarism-detection/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/plagiarism-detection/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/plagiarism-detection/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/plagiarism-detection/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/plagiarism-detection/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "41a0c49321369f5eacc74229fabed7d8",
+    "url": "/plagiarism-detection/static/media/team1.41a0c493.jpg"
+  },
+  {
+    "revision": "f28369d29e3455da1c031b13fab49d17",
+    "url": "/plagiarism-detection/static/media/team2.f28369d2.jpg"
+  },
+  {
+    "revision": "ca56eda3968dcb1a6efc24a9a66f7cf3",
+    "url": "/plagiarism-detection/static/media/team3.ca56eda3.jpg"
+  },
+  {
+    "revision": "a8478b4754e1fde6beec0fa6b44cf212",
+    "url": "/plagiarism-detection/static/media/team4.a8478b47.png"
+  },
+  {
+    "revision": "9ecfc4a002e7bee2f63321ccce8d3c79",
+    "url": "/plagiarism-detection/static/media/team5.9ecfc4a0.jpg"
+  }
+]);
